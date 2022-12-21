8b3fad909712c01a908c75ef1fe5dc0f9567a17b
kimhw7 <khw786678@gmail.com>
2022-12-21 11:30:05 +0900

    #19 - 컴포넌트 초기 정렬
    화면 확인용 컴포넌트 초기 정렬

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import MainPage from './pages/MainPage';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <MainPage />
     </div>
   );
 }
---
+++ client/src/components/Nav.js
@@ -0,0 +1,7 @@
+const Nav = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default Nav;
