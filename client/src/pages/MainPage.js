71f515933e974a362810f1b77b658d480e773288
kimhw7 <khw786678@gmail.com>
2022-12-21 11:30:05 +0900

    #19 - 컴포넌트 초기 정렬
    화면 확인용 컴포넌트 초기 정렬

---
+++ client/src/pages/MainPage.js
@@ -0,0 +1,13 @@
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
+
+const MainPage = () => {
+  return (
+    <div>
+      <Nav />
+      <Footer />
+    </div>
+  )
+}
+
+export default MainPage;
