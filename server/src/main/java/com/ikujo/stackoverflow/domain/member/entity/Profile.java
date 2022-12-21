5f64806e0ced79e0ad732b93b874288fef5aef9e
kurt <qodydgus97@kakao.com>
2022-12-21 19:01:17 +0900

    #18 - JPA 연관관계 매핑

---
+++ server/src/main/java/com/ikujo/stackoverflow/domain/member/entity/Profile.java
@@ -0,0 +1,21 @@
+package com.ikujo.stackoverflow.domain.member.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class Profile {
+
+    private String location;
+
+    @Column(length = 30)
+    private String title;
+
+    private String aboutMe;
+
+}
