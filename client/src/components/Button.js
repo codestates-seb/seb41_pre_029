34375b43e766a79fb6cd06f4237c7db3123d7cb4
kimhw7 <khw786678@gmail.com>
2022-12-21 16:53:17 +0900

    #24 Button 컴포넌트 구현
    버튼 이름과 링크, css 설정을 입력받아 생성되는 버튼 컴포넌트 구현

---
+++ client/src/components/Button.js
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  width: ${(props) => props.width || "80px"};
+  height: 33px;
+  outline: none;
+  margin: 0 3px;
+  padding: 8px 10.4px;
+  border-radius: 5px;
+  border: 1px solid #39739d;
+  color: ${(props) => props.color || "#FFFFFF"};
+  background-color: ${(props) => props.background || "#0A95FF"};
+`;
+
+const handleClick = () => {
+  
+}
+
+const Button = ({ buttonName, link, ...rest }) => {
+  return(
+    <Link to={link}>
+      <StyledButton {...rest} onClick={handleClick}>buttonName</StyledButton>
+    </Link>
+  )
+};
+
+export default Button;
