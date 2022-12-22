144a7bf6a435b327cea78651494b67d0d3bd66bd
kimhw7 <khw786678@gmail.com>
2022-12-23 00:32:16 +0900

    #9 마이페이지 구현
    서버 연동, 컴포넌트 랜더링 미완성

--- client/src/pages/MyPage.js
+++ client/src/pages/MyPage.js
@@ -1,3 +1,4 @@
+// about 내용, 아이콘 날짜 정보 받아와야 함
 import styled from "styled-components";
 import { useState } from "react";
 
@@ -32,13 +33,25 @@ const ContentsWrapper = styled.div `
         margin-left: 5px;
         margin-bottom: 12px;
       }
+      > .icons {
+        display: flex;
+        color: #6a737c;
+        > .icons-wrapper {
+          display: flex;
+          align-items: center;
+          margin: 4px;
+          > svg {
+            margin: 0 2px 0 2px;
+          }
+        }
+      }
     }
   }
 
   // nav, 인터페이스 부분
   > .content-body {
     display: flex;
-    height: 100%;
+    /* height: 100%; */
     > nav {
       margin-right: 32px;
       > ul {
@@ -57,12 +70,14 @@ const ContentsWrapper = styled.div `
           margin-top: 30px;
         }
         > .button {
+          cursor: pointer;
           padding: 6px 48px 6px 12px;
           display: block;
           align-items: center;
           border-radius: 15px;
           color: #525960;
           font-weight: 600;
+          white-space: nowrap;
           :hover {
             background-color: #e3e6e8;
           }
@@ -80,26 +95,70 @@ const ContentsWrapper = styled.div `
         }
       }
     }
+    > .content-wrapper {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      > .content-about {
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 24px;
+        > .content-about--title {
+          font-size: 21px;
+          font-weight: 400;
+          text-align: left;
+          margin-bottom: 8px;
+        }
+        > .content-about--body {
+          border-radius: 5px;
+          background-color: #f8f9f9;
+          border: solid 1px hsl(210deg 8% 85%);
+          text-align: center;
+          padding: 32px;
+          color: #6a737c;
+          font-weight: 400;
+          font-size: 13px;
+          > p {
+            margin: 0 200px 0 200px;
+          }
+        }
+      }
+      > .content-interface {
+
+      }
+    }
   }
 `
 
 const MyPage = () => {
+  const [ activeTheme, setActiveTheme ] = useState(true);
   const [ activeEdit, setActiveEdit ] = useState(false);
   const [ activeDel, setActiveDel ] = useState(false);
-  const [ page, setPage ] = useState('nothing');
+  const [ page, setPage ] = useState('theme');
 
   const handleClickEdit = () => {
     if(!activeEdit) {
       setActiveEdit(true);
       setActiveDel(false);
+      setActiveTheme(false);
       setPage('edit');
     }
   }
   const handleClickDel = () => {
     if(!activeDel) {
       setActiveDel(true);
+      setActiveTheme(false);
       setActiveEdit(false);
       setPage('delete');
+    }
+  }
+
+  const handleClickTheme = () => {
+    if(!activeTheme) {
+      setActiveTheme(true);
+      setActiveDel(false);
+      setActiveEdit(false);
+      setPage('theme');
     }
   }
 
@@ -113,7 +172,18 @@ const MyPage = () => {
           <div className="content-userBar--flexItems">
             <div className="username">{'userName'}</div>
             <ul className="icons">
-              <li></li>
+              <li className="icons-wrapper">
+                <svg fill="#6a737c" aria-hidden="true" className="svg-icon iconCake" width="18" height="18" viewBox="0 0 18 18"><path d="M9 4.5a1.5 1.5 0 0 0 1.28-2.27L9 0 7.72 2.23c-.14.22-.22.48-.22.77 0 .83.68 1.5 1.5 1.5Zm3.45 7.5-.8-.81-.81.8c-.98.98-2.69.98-3.67 0l-.8-.8-.82.8c-.49.49-1.14.76-1.83.76-.55 0-1.3-.17-1.72-.46V15c0 1.1.9 2 2 2h10a2 2 0 0 0 2-2v-2.7c-.42.28-1.17.45-1.72.45-.69 0-1.34-.27-1.83-.76Zm1.3-5H10V5H8v2H4.25C3 7 2 8 2 9.25v.9c0 .81.91 1.47 1.72 1.47.39 0 .77-.14 1.03-.42l1.61-1.6 1.6 1.6a1.5 1.5 0 0 0 2.08 0l1.6-1.6 1.6 1.6c.28.28.64.43 1.03.43.81 0 1.73-.67 1.73-1.48v-.9C16 8.01 15 7 13.75 7Z"/></svg>
+                <span>Member for {9} months</span>
+              </li>
+              <li className="icons-wrapper">
+                <svg fill="#6a737c" aria-hidden="true" className="svg-icon iconClock" width="18" height="18" viewBox="0 0 18 18"><path d="M9 17c-4.36 0-8-3.64-8-8 0-4.36 3.64-8 8-8 4.36 0 8 3.64 8 8 0 4.36-3.64 8-8 8Zm0-2c3.27 0 6-2.73 6-6s-2.73-6-6-6-6 2.73-6 6 2.73 6 6 6ZM8 5h1.01L9 9.36l3.22 2.1-.6.93L8 10V5Z"/></svg>
+                <span>Last seen {'this week'}</span>
+              </li>
+              <li className="icons-wrapper">
+                <svg fill="#6a737c" aria-hidden="true" className="svg-icon iconCalendar" width="18" height="18" viewBox="0 0 18 18"><path d="M14 2h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4c0-1.1.9-2 2-2h1V0h2v2h6V0h2v2ZM3 6v9h12V6H3Zm2 2h2v2H5V8Zm0 3h2v2H5v-2Zm3 0h2v2H8v-2Zm3 0h2v2h-2v-2Zm0-3h2v2h-2V8ZM8 8h2v2H8V8Z"/></svg>
+                <span>Visited {7} days ago</span>
+              </li>
             </ul>
           </div>
         </div>
@@ -122,6 +192,9 @@ const MyPage = () => {
             <ul>
               <li className="title">
                 <span>PERSONAL INFORMATION</span>
+              </li>
+              <li className={`button ${activeTheme ? 'active' : ''}`} onClick={handleClickTheme}>
+                <span>Theme</span>
               </li>
               <li className={`button ${activeEdit ? 'active' : ''}`} onClick={handleClickEdit}>
                 <span>Edit profile</span>
@@ -174,9 +247,14 @@ const MyPage = () => {
             </ul>
           </nav>
           <div className="content-wrapper">
-            <div className="content-about"></div>
+            <div className="content-about">
+              <div className="content-about--title">About</div>
+              <div className="content-about--body">
+                <p>자꾸 눈이 감긴다.Your about me section is currently blank. Would you like to add one? Edit profile</p>
+              </div>
+            </div>
             <div className="content-interface">
-              {page === 'nothing' && <div></div>}
+              {page === 'theme' && <div>theme</div>}
               {page === 'edit' && <EditProfile />}
               {page === 'delete' && <DeleteProfile />}
             </div>
