79875516fd9d415e7c03be651c3f69c345140a23
greatshine917 <j01039519778@gmail.com>
2022-12-23 14:00:32 +0900

    #48 답글 등록
    답글 등록에 필요한 비즈니스 로직과 컨트롤러 post 메서드 구현

--- server/src/main/java/com/ikujo/stackoverflow/domain/comment/controller/CommentController.java
+++ server/src/main/java/com/ikujo/stackoverflow/domain/comment/controller/CommentController.java
@@ -18,32 +18,7 @@ import java.util.List;
 @RequestMapping("/questions/{article-id}/comments")
 public class CommentController {
 
-    @GetMapping
-    public ResponseEntity getComments(@PathVariable("article-id") Long id) {
-
-        Member member = new Member(1L,
-                "aaa@gmail.com",
-                "1234",
-                "김회원",
-                new Profile("대한민국", "안녕하세요", "김회원입니다~"));
-        Article article = new Article(1L, member, "블라블라블라", null, 0, null);
-
-        Comment comment = new Comment(1L, article, member, "블라블라블라1", false, 0, null);
-        Comment comment1 = new Comment(2L, article, member, "블라라2", false, 0, null);
-        Comment comment2 = new Comment(3L, article, member, "블라블라3", false, 0, null);
-
-        CommentResponseDto commentResponseDto = CommentResponseDto.of(comment);
-        CommentResponseDto commentResponseDto1 = CommentResponseDto.of(comment1);
-        CommentResponseDto commentResponseDto2 = CommentResponseDto.of(comment2);
-
-        List<CommentResponseDto> commentResponseDtoList = new ArrayList<>();
-
-        commentResponseDtoList.add(commentResponseDto);
-        commentResponseDtoList.add(commentResponseDto1);
-        commentResponseDtoList.add(commentResponseDto2);
-
-        return new ResponseEntity<>(
-                new CommentMultiResponseDto<>(commentResponseDtoList.size(),commentResponseDtoList), HttpStatus.OK);
+     // 작업 시작
 
     }
-}
+
