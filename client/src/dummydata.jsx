0f84f4e38443451ac9291055e8d078335f7cec19
kimhw7 <khw786678@gmail.com>
2022-12-24 19:32:56 +0900

    #54 question page 기본틀, Nav선택 뷰 변경, dummydata변경
    Nav question 페이지에서 선택된 형태로 안나오는 부분 수정, 서버에서 받아오는 형태와 동일하게 더미데이터 수정

--- client/src/dummydata.jsx
+++ client/src/dummydata.jsx
@@ -1,132 +1,80 @@
-//주석
 const data = [
-  {
-    id: 415,
-    createdAt: "2022-05-16T01:02:17Z",
-    updatedAt: "2022-05-16T01:02:17Z",
-    title: "제목을 넣어주세요1",
-    nickname: "아이디 자리",
-    content:
-      "I have a folder of videos and I want to take a jpeg every 60 seconds from those videos using cv2, while giving names the code listed below I only manage to get 1 screenshot fro...",
-    userAvatar: "🥺",
-    likes: 0,
-    tags: ["1c", "1java"],
-    views: 0,
-    answer: [
       {
-        userId: 123,
-        userAvatar: "🤔",
-        content: "asdfdsada",
-        createdAt: "2022-05-16T01:02:17Z",
-        isChecked: false,
-        likes: 0
+          member: {
+              nickname: "1쿠조",
+              memberId: 1
+          },
+          id: 1,
+          title: "제목 입니다1제목 입니다1제목 입니다1제목 입니다1",
+          content: "내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1",
+          tags: [
+              "#third",
+              "#java",
+              "#spring",
+              "#like"
+          ],
+          recommendCount: 1,
+          hits: 1,
+          baseTime: {
+              createdAt: "2022-12-24T14:26:59.7627641",
+              lastModifiedAt: "2022-12-24T14:26:59.7627641"
+          },
+          selection: true,
+          commentCount: 1
       },
       {
-        userId: 1123,
-        userAvatar: "🤔",
-        content: "asdfdsada",
-        createdAt: "2022-12-16T01:02:17Z",
-        isChecked: false,
-        likes: 5
-      }
-    ]
-  },
+        member: {
+            nickname: "2쿠조",
+            memberId: 2
+        },
+        id: 2,
+        title: "제목 입다1제목 입니1니다222222222222222222목 입니다1제목 입니다1제목 입니다1제목 입니다1제목 입니다1니다1",
+        content: "내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1",
+        tags: [
+            "#third",
+            "#java",
+            "#spring",
+            "#like"
+        ],
+        recommendCount: 1,
+        hits: 1,
+        baseTime: {
+            createdAt: "2022-12-24T14:26:59.7627641",
+            lastModifiedAt: "2022-12-24T14:26:59.7627641"
+        },
+        selection: true,
+        commentCount: 1
+    },
     {
-    id: 4125,
-    createdAt: "2022-05-16T01:02:17Z",
-    updatedAt: "2022-05-16T01:02:17Z",
-    title: "제목을 넣어주세요1",
-    nickname: "아이디 자리",
-    content:
-      "I have a folder of videos and I want to take a jpeg every 60 seconds from those videos using cv2, while giving names the code listed below I only manage to get 1 screenshot fro...",
-    userAvatar: "🥺",
-    likes: 0,
-    tags: ["#C", "#JAVA"],
-    views: 0,
-    answer: [
-      {
-        userId: 12233,
-        userAvatar: "🤔",
-        content: "asdfdsada",
-        createdAt: "2022-05-16T01:02:17Z",
-        isChecked: false,
-        likes: 0,
-      },
-      {
-        userId: 1123,
-        userAvatar: "🤔",
-        content: "asdfdsada",
-        createdAt: "2022-12-16T01:02:17Z",
-        isChecked: false,
-        likes: 5
-      }
-    ]
-  },
-    {
-    id: 41295,
-    createdAt: "2022-05-16T01:02:17Z",
-    updatedAt: "2022-05-16T01:02:17Z",
-    title: "제목을 넣어주세요1",
-    nickname: "아이디 자리",
-    content:
-      "I have a folder of videos and I want to take a jpeg every 60 seconds from those videos using cv2, while giving names the code listed below I only manage to get 1 screenshot fro...",
-    userAvatar: "🥺",
-    likes: 0,
-    tags: ["c", "java"],
-    views: 0,
-    recommendCount:1,
-    answer: [
-      {
-        userId: 1823,
-        userAvatar: "🤔",
-        content: "asdfdsada",
-        createdAt: "2022-05-16T01:02:17Z",
-        isChecked: false,
-        likes: 0
-      },
-      {
-        userId: 23,
-        userAvatar: "🤔",
-        content: "asdfdsada",
-        createdAt: "2022-12-16T01:02:17Z",
-        isChecked: false,
-        likes: 5
-      }
-    ]
-  },
-    {
-    id: 41,
-    createdAt: "2022-05-16T01:02:17Z",
-    updatedAt: "2022-05-16T01:02:17Z",
-    title: "제목을 넣어주세요1",
-    nickname: "아이디 자리",
-    content:
-      "I have a folder of videos and I want to take a jpeg every 60 seconds from those videos using cv2, while giving names the code listed below I only manage to get 1 screenshot fro...",
-    userAvatar: "🥺",
-    likes: 0,
-    tags: ["c", "java"],
-    views: 0,
-    selection:true,
-    recommendCount:1,
-    answer: [
-      {
-        userId: 12123,
-        userAvatar: "🤔",
-        content: "asdfdsada",
-        createdAt: "2022-05-16T01:02:17Z",
-        isChecked: false,
-        likes: 0
-      },
-      {
-        userId: 1121233,
-        userAvatar: "🤔",
-        content: "asdfdsada",
-        createdAt: "2022-12-16T01:02:17Z",
-        isChecked: false,
-        likes: 5
-      }
-    ]
+        member: {
+            nickname: "3쿠조",
+            memberId: 2
+        },
+        id: 2,
+        title: "제목 33333입333333다1제목 입니1니33333333333입니다1제목 입니다1제목 입니다1제목 입니다1제목 입니다1니다1",
+        content: "내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1내용 입니다1",
+        tags: [
+            "#third",
+            "#java",
+            "#spring",
+            "#like"
+        ],
+        recommendCount: 1,
+        hits: 1,
+        baseTime: {
+            createdAt: "2022-12-24T14:26:59.7627641",
+            lastModifiedAt: "2022-12-24T14:26:59.7627641"
+        },
+        selection: true,
+        commentCount: 1
+    }
+  ]
+  
+  const pageInfo = {
+      page: 1,
+      size: 10,
+      totalElements: 300,
+      totalPages: 30
   }
-];
 
-export default data;
+  export default data;
--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/questions",
     createProxyMiddleware({
-      target: "https://d462-116-37-88-211.jp.ngrok.io",
+      target: "https://b6e2-219-240-149-145.jp.ngrok.io",
       changeOrigin: true,
     })
   );
