f582cf4e309ff08175284a9b36bed951731db76b
Sungho95 <tjdgh1785@gmail.com>
2022-12-25 19:00:53 +0900

    #66 - 임시 컨트롤러 수정
    DTO 변경으로 인한 컨트롤러 로직 수정

--- server/src/main/java/com/ikujo/stackoverflow/domain/member/controller/MemberController.java
+++ server/src/main/java/com/ikujo/stackoverflow/domain/member/controller/MemberController.java
@@ -2,9 +2,8 @@ package com.ikujo.stackoverflow.domain.member.controller;
 
 import com.ikujo.stackoverflow.domain.member.entity.Member;
 import com.ikujo.stackoverflow.domain.member.entity.Profile;
-import com.ikujo.stackoverflow.domain.member.entity.dto.MemberProfilePatchDto;
-import com.ikujo.stackoverflow.domain.member.entity.dto.MemberResponseDto;
-import com.ikujo.stackoverflow.domain.member.repository.StubMemberRepository;
+import com.ikujo.stackoverflow.domain.member.entity.dto.request.MemberProfilePatch;
+import com.ikujo.stackoverflow.domain.member.entity.dto.response.MemberResponse;
 import com.ikujo.stackoverflow.global.dto.SingleResponseDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/members")
 @RequiredArgsConstructor
@@ -25,14 +25,14 @@ public class MemberController {
                 "김회원",
                 new Profile("대한민국", "안녕하세요", "김회원입니다~"));
 
-        MemberResponseDto memberResponseDto = MemberResponseDto.of(member);
+        MemberResponse memberResponseDto = MemberResponse.of(member);
 
         return new ResponseEntity<>(new SingleResponseDto<>(memberResponseDto), HttpStatus.OK);
     }
 
     @PatchMapping("/{id}")
     public ResponseEntity patchMemberProfile(@PathVariable("id") Long id,
-                                             @Valid @RequestBody MemberProfilePatchDto profilePatchDto) {
+                                             @Valid @RequestBody MemberProfilePatch profilePatchDto) {
 
         Member member = new Member(1L,
                 "aaa@gmail.com",
@@ -40,9 +40,9 @@ public class MemberController {
                 "김회원",
                 new Profile("대한민국", "안녕하세요", "김회원입니다~"));
 
-        MemberResponseDto memberResponseDto = MemberResponseDto.of(member);
+        MemberResponse memberResponse = MemberResponse.of(member);
 
-        return new ResponseEntity<>(new SingleResponseDto<>(memberResponseDto), HttpStatus.OK);
+        return new ResponseEntity<>(new SingleResponseDto<>(memberResponse), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
