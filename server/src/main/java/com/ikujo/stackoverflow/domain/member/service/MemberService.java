2135acb6780bef23745a5c9999f6fedbed808c3f
Sungho95 <tjdgh1785@gmail.com>
2022-12-25 23:45:46 +0900

    #66 - MemberService 작성
    createMember
    - 회원 가입 로직
    - 이메일 중복 검증 로직 사용
    - 이메일 인증로직 필요함
    
    updateMember
    - 회원 프로필 수정 로직
    - 레코드 사용X (리팩토링 필요!)
    
    loginMember
    - 회원 로그인 로직
    - 이메일 검증만 함.
    - 비밀번호 일치 여부 필요
    
    deleteMember
    - 회원 삭제 로직
    
    findMember
    - 회원 조회 로직
    - 회원 조회 검증 로직 사용
    
    verifyExistsEmail
    - 이메일 중복 검증 로직
    
    findVerifiedMember
    - 회원 조회 검증 로직

--- server/src/main/java/com/ikujo/stackoverflow/domain/member/service/MemberService.java
+++ server/src/main/java/com/ikujo/stackoverflow/domain/member/service/MemberService.java
@@ -1,14 +1,115 @@
 package com.ikujo.stackoverflow.domain.member.service;
 
+import com.ikujo.stackoverflow.domain.member.entity.Member;
+import com.ikujo.stackoverflow.domain.member.entity.Profile;
+import com.ikujo.stackoverflow.domain.member.entity.dto.MemberDto;
+import com.ikujo.stackoverflow.domain.member.entity.dto.request.MemberLoginPost;
+import com.ikujo.stackoverflow.domain.member.entity.dto.request.MemberProfilePatch;
+import com.ikujo.stackoverflow.domain.member.entity.dto.request.MemberSignupPost;
+import com.ikujo.stackoverflow.domain.member.entity.dto.response.MemberResponse;
+import com.ikujo.stackoverflow.domain.member.repository.MemberRepository;
 import com.ikujo.stackoverflow.domain.member.repository.StubMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class MemberService {
 
-    private final StubMemberRepository stubMemberRepository;
+    private final MemberRepository memberRepository;
 
+    /**
+     * 회원 가입
+     */
+    public Member createMember(Member member) {
+        verifyExistsEmail(member.getEmail());
+        Member savedMember = memberRepository.save(member);
 
+        // 이메일 인증 로직 필요
+
+        return savedMember;
+    }
+
+    /**
+     * 회원 프로필 수정 (리팩토링 필요!)
+     */
+    @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.SERIALIZABLE)
+    public Member updateMember(Long id, MemberProfilePatch memberProfilePatch) {
+        Member findMember = findVerifiedMember(id);
+
+        if (findMember.getProfile() == null) {
+            findMember.setProfile(new Profile());
+        }
+
+        // 닉네임은 필수
+        Optional.of(memberProfilePatch.nickname())
+                .ifPresent(findMember::setNickname);
+
+        // 프로필 정보는 null 허용
+        Optional.ofNullable(memberProfilePatch.location())
+                .ifPresent(location -> findMember.getProfile().setLocation(location));
+
+        Optional.ofNullable(memberProfilePatch.title())
+                .ifPresent(title -> findMember.getProfile().setTitle(title));
+
+        Optional.ofNullable(memberProfilePatch.aboutMe())
+                .ifPresent(aboutMe -> findMember.getProfile().setAboutMe(aboutMe));
+
+        return memberRepository.save(findMember);
+    }
+
+    /**
+     * 회원 로그인 (리팩토링 필요!!)
+     */
+    public void loginMember(MemberLoginPost memberLoginPost) {
+        Optional<Member> findMember = memberRepository.findByEmail(memberLoginPost.email());
+        if (!findMember.isPresent()) {
+            throw new RuntimeException("존재하지 않는 이메일입니다.");
+        }
+    }
+
+    /**
+     * 회원 삭제
+     */
+    public void deleteMember(Long id) {
+        Member findMember = findVerifiedMember(id);
+
+        memberRepository.delete(findMember);
+    }
+
+    /**
+     * 회원 조회
+     */
+    @Transactional(readOnly = true)
+    public Member findMember(Long id) {
+        return findVerifiedMember(id);
+    }
+
+    /**
+     * 이메일 중복 검증
+     */
+    private void verifyExistsEmail(String email) {
+        Optional<Member> member = memberRepository.findByEmail(email);
+        if (member.isPresent())
+            throw new RuntimeException("같은 이메일이 존재합니다.");
+    }
+
+    /**
+     * 회원 조회 검증
+     */
+    @Transactional(readOnly = true)
+    public Member findVerifiedMember(Long id) {
+        Optional<Member> findMember = memberRepository.findById(id);
+
+        Member member = findMember.orElseThrow(() ->
+                new RuntimeException("잘못된 회원 정보입니다."));
+
+        return member;
+    }
 }
