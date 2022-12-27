a3827160e5ecdef0d89f43373d576fd9988bf946
Sungho95 <tjdgh1785@gmail.com>
2022-12-27 14:10:12 +0900

    #93 - ExceptionCode 이메일 검증 에러 추가
    이메일을 찾지 못한 경우
    중복된 이메일인 경우

--- server/src/main/java/com/ikujo/stackoverflow/global/exception/ExceptionCode.java
+++ server/src/main/java/com/ikujo/stackoverflow/global/exception/ExceptionCode.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 public enum ExceptionCode {
     MEMBER_NOT_FOUND(404, "Member not found"),
     MEMBER_EXISTS(409, "Member exists"),
+    EMAIL_NOT_FOUND(404, "Email not found"),
+    EMAIL_EXISTS(409, "Email exists"),
     ARTICLE_NOT_FOUND(404, "Article not found"),
     ARTICLE_EXISTS(409, "Article exists"),
     COMMENT_NOT_FOUND(404, "Comment not found");
