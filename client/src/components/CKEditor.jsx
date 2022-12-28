9585e930ecc34540918d83b4234124c54abddbf2
kimhw7 <khw786678@gmail.com>
2022-12-28 15:32:02 +0900

    117 에디터 레이아웃, css 수정

--- client/src/components/CKEditor.jsx
+++ client/src/components/CKEditor.jsx
@@ -1,36 +1,58 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MDEditor from '@uiw/react-md-editor';
 import styled from "styled-components";
 
 const Container = styled.div `
+  height: auto;
   .w-md-editor {
-  overflow: visible;
+  overflow: initial;
   display: flex;
   flex-direction: column;
-  height: 100%;
+  height: auto;
+  background-color: #ffffff;
 
   > .w-md-editor-content {
+    min-height: 400px;
     display: flex;
     flex-direction: column;
     width: 100%;
-    height: 100%;
+    height: auto;
+    overflow: initial;
+    background-color: #ffffff;
 
     > .w-md-editor-input {
       color: black;
       background-color: #ffffff;
+      overflow: initial;
       width: 100%;
+      min-height: 200px;
+      height: auto;
+
+      > .w-md-editor-text {
+        min-height: 200px;
+        height: auto;
+        line-height: 30px;
+      }
+
     }
     > .w-md-editor-preview  {
       color: black;
       background-color: #ffffff;
       width: 100%;
       position: relative;
-      border: 1px solid black;
+  
+      overflow: initial;
+      min-height: 200px;
+      height: auto;
+      box-shadow: none;
+      border-top: 1px solid rgba(0, 0, 0, 0.2);
 
         > .wmde-markdown-color {
         color: black;
         background-color: #ffffff;
-        overflow: visible;
+        overflow: initial;
+        height: auto;
+        border: none;
       }
     }
   }
@@ -43,14 +65,22 @@ const CEditor = ({onChange, data}) => {
   const changeValue = (e) => {
     setValue(e)
   }
+  useEffect(() => {
+    onChange(value)
+  }, [value])
   return (
     <Container>
       <MDEditor
+        style={{
+          backgroundColor: 'white',
+      }}
+        height={'auto'}
+        minHeight={700}
+        
         value={value}
         onChange={
           (e) => {
-          changeValue(e)
-          onChange(value)
+            changeValue(e)
           }
         }
       />
