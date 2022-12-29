e83b8d4b78d9ad3f7af9b6e138a37e1e63f14ddf
greatshine917 <j01039519778@gmail.com>
2022-12-29 17:45:29 +0900

    #146 답글 채택 구현
    답글 채택 로직을 추가했습니다.

--- server/src/main/java/com/ikujo/stackoverflow/domain/comment/dto/CommentDto.java
+++ server/src/main/java/com/ikujo/stackoverflow/domain/comment/dto/CommentDto.java
@@ -3,6 +3,7 @@ package com.ikujo.stackoverflow.domain.comment.dto;
 import com.ikujo.stackoverflow.domain.article.entity.Article;
 import com.ikujo.stackoverflow.domain.comment.dto.request.CommentPatch;
 import com.ikujo.stackoverflow.domain.comment.dto.request.CommentPost;
+import com.ikujo.stackoverflow.domain.comment.dto.request.CommentSelection;
 import com.ikujo.stackoverflow.domain.comment.entity.Comment;
 import com.ikujo.stackoverflow.domain.comment.entity.CommentRecommend;
 import com.ikujo.stackoverflow.domain.member.entity.Member;
@@ -28,6 +29,11 @@ public record CommentDto(Long id,
     public static CommentDto of(Comment comment, CommentPatch commentPatch) {
         return new CommentDto(comment.getId(), comment.getArticle(), comment.getMember(), commentPatch.content(),
                comment.getSelection() , comment.getCreatedAt(), null);
+    }
+
+    public static CommentDto of(Comment comment, CommentSelection commentSelection) {
+        return new CommentDto(comment.getId(), comment.getArticle(), comment.getMember(), comment.getContent(),
+                commentSelection.selection(), comment.getCreatedAt(), null);
     }
 
     public Comment toEntity() {
