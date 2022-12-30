aa1289ecc04c17bcee20c7122605f7c0d63d9e44
SEB_BE_41_조대희 <111113889+greatshine917@users.noreply.github.com>
2022-12-31 01:10:53 +0900

    #97 답글 좋아요 구현
    답글 좋아요 기능 response 수정

--- server/src/test/java/com/ikujo/stackoverflow/domain/comment/controller/CommentControllerTest.java
+++ server/src/test/java/com/ikujo/stackoverflow/domain/comment/controller/CommentControllerTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
@@ -19,6 +20,7 @@ import java.time.LocalDateTime;
 
 @WebMvcTest(MemberController.class)
 @MockBean(JpaMetamodelMappingContext.class)
+@AutoConfigureRestDocs
 public class CommentControllerTest {
 
     @Autowired
