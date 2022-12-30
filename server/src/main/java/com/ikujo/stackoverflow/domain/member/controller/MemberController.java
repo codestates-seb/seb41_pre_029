6ee667395b5937b9785de8a26c60dddd394c6204
Sungho95 <tjdgh1785@gmail.com>
2022-12-30 18:54:52 +0900

    #132 - 회원 도메인 인증 수정
    이번엔 될거야..

--- server/src/main/java/com/ikujo/stackoverflow/domain/member/controller/MemberController.java
+++ server/src/main/java/com/ikujo/stackoverflow/domain/member/controller/MemberController.java
@@ -35,6 +35,7 @@ public class MemberController {
     @GetMapping("/{id}")
     public ResponseEntity getMemberProfile(@PathVariable("id") Long id,
                                            @RequestHeader(name = "Authorization") String token) {
+        memberService.verifyId(id, token);
         Member findMember = memberService.findMemberByToken(token);
         MemberResponse response = MemberResponse.from(findMember);
 
@@ -48,7 +49,7 @@ public class MemberController {
     public ResponseEntity patchMemberProfile(@PathVariable("id") Long id,
                                              @Valid @RequestBody MemberProfilePatch profilePatchDto,
                                              @RequestHeader(name = "Authorization") String token) {
-
+        memberService.verifyId(id, token);
         Member updateMember = memberService.updateMember(token, profilePatchDto);
 
         MemberResponse response = MemberResponse.from(updateMember);
@@ -62,7 +63,8 @@ public class MemberController {
     @DeleteMapping("/{id}")
     public ResponseEntity deleteMember(@PathVariable("id") Long id,
                                        @RequestHeader(name = "Authorization") String token) {
-        memberService.deleteMember(token);
+        memberService.verifyId(id, token);
+        memberService.deleteMember(id);
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
